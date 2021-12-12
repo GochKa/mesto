b476920c99321a826c11f04702eedda9e09107fa
GochKa <gocha.workout@gmail.com>
2021-12-12 23:38:28 +0300

    JS
    Добавлен обработчик Popup'а

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+const popupOpenButton = document.querySelector(".edit-button");
+const popupCloseButton = document.querySelector(".popup__close");
+const popup = document.querySelector(".popup");
+
+
+function togglePopup(){
+    popup.classList.toggle("popup_opened");
+}
+
+function closePopupOnOverlayClick(event){
+    if (event.target === event.currentTarget){
+        popup.classList.remove("popup_opened")
+    }
+
+}
+
+popupOpenButton.addEventListener("click", togglePopup);
+popupCloseButton.addEventListener("click", togglePopup);
+popup.addEventListener("click", closePopupOnOverlayClick)
